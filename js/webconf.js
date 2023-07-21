1165fd673cd8706fcd749bf961675193770377bf
joaquim <joaquim.claudio2003@gmail.com>
2023-07-21 05:04:37 +0100

    Starting the front-end component of WebConference project.

---
+++ js/webconf.js
@@ -0,0 +1,46 @@
+window.onload = function() {
+    const btnRegister = document.getElementById("btnRegister");
+    btnRegister.addEventListener("click", function(){
+        //Abertura da janela modal
+        swal ({
+            title: "Inscrição na WebConference",
+            html:
+                '<input id="txtName" class="swal2-input" placeholder ="Nome (primeiro e último)">' +
+                '<input id="txtEmail" class="swal2-input" placeholder ="E-mail: alguem@exemplo.com">',
+            showCancelButton: true,
+            confirmButtonText: "Inscrever",
+            cancelButtonText: "Cancelar",
+            showLoaderOnConfirm: true,
+            preConfirm: () => {
+                const name = document.getElementById("txtName").value;
+                const email = document.getElementById("txtEmail").value;
+                const url_base = "https://fcawebbook.herokuapp.com";
+                return
+                    fetch (`${url_base}/conferences/1/participants/${email}`,{
+                        headers: { "content-Type": "application/x-www-form-urlencoded" },
+                        method: "POST",
+                        body: `nomeparticipant=${name}`
+                    })
+                    .then(response => {
+                        if(!response.ok){
+                            throw new Error(response.statusText);
+                        }
+                        return response.json();
+                    })
+                    .catch(error => {
+                        swal.showValidationError(`Pedido falhou: ${error}`);
+                    });
+            },
+            allouOutsideClick: () => !swal.isLoading()
+
+        }).then(result => {
+            if(result.value){
+                if(!result.value.err_code) {
+                    swal({title: "Inscrição feita com sucesso!"})
+                }else{
+                    swal({title: `${result.value.err_message}`})
+                }
+            }
+        })
+    })
+}
